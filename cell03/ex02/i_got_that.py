f5e71768d90309453fa1576adbf8406fad3faca3
Tuner <127730775+TUNERXD@users.noreply.github.com>
2025-06-24 04:46:56 +0000

    3.2 added i_got_that.py

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+user_input = input("What you gotta say? : ")
+
+while True:
+    user_input = input("I got that! Anything else? : ")
+
+    if user_input == "STOP":
+        break
