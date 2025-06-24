13d52ed6259f2d3828c81431522d98ed5dc21481
Tuner <127730775+TUNERXD@users.noreply.github.com>
2025-06-24 12:35:25 +0000

    6.5 scope_that.py

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def main():
+
+    variable = 0
+    print(variable)
+    add_one(variable)
+    print(add_one(variable))
+
+
+def add_one(i):
+    return i + 1
+
+if __name__ == "__main__":
+    main()
