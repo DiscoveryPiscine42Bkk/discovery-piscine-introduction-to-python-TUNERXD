7781011aed7addb190497f0991d961de4f39839c
Tuner <127730775+TUNERXD@users.noreply.github.com>
2025-06-24 09:03:32 +0000

    5.11 added count_it.py

---
+++ cell05/ex11/count_it.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+
+if len(sys.argv) < 2:
+    print("none")
+    sys.exit(1)
+
+print(f"parameters: {len(sys.argv)}")
+for word in sys.argv[1:]:
+    print(f"{word}: {len(word)}")
+
