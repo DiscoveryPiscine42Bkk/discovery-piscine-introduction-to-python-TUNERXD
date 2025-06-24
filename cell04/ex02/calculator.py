1bdf62789c3cbbae97cc7da8688b4015b0d652bf
Tuner <127730775+TUNERXD@users.noreply.github.com>
2025-06-24 13:10:52 +0000

    Handles zerodivision edge case

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -4,10 +4,16 @@ try:
     
     num1 = int(input("Give me the first number: "))
     num2 = int(input("Give me the second number: "))
+
     print("Thank you!")
     print(f"{num1} + {num2} = {num1 + num2}")
     print(f"{num1} - {num2} = {num1 - num2}")
-    print(f"{num1} / {num2} = {round(num1 / num2)}")
+
+    if num2 != 0:
+        print(f"{num1} / {num2} = {round(num1 / num2)}")
+    else:
+        print("Denominator can't be zero")
+
     print(f"{num1} x {num2} = {num1 * num2}")
 
 except ValueError:
