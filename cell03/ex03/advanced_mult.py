9d9ad12e241d8c3043de63ffce93f0a98b32ed06
Tuner <127730775+TUNERXD@users.noreply.github.com>
2025-06-24 05:13:24 +0000

    added 3.3 advanced_mult.py

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sys
+
+
+def main():
+
+    if len(sys.argv) > 1:
+        print("none")
+        return
+
+
+    total_table = 10
+    total_num = 10
+    current_table = 0
+    current_num = 0
+
+    while current_table <= total_table:
+        print(f"Table de {current_table}: ", end = '')
+        while current_num <= total_num:
+            result = current_table * current_num
+            print(f"{result} ", end = '')
+            current_num += 1
+        current_num = 0 # reset current_num to 0
+        current_table += 1 # increment to next table
+        print()
+
+if __name__ == "__main__":
+    main()
