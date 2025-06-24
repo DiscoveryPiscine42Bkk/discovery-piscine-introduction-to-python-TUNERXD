19d2d48217480771a630ad1b85c0bc16b56a2068
Tuner <127730775+TUNERXD@users.noreply.github.com>
2025-06-24 04:18:11 +0000

    2.3 added mult.py

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+try:
+    num1 = int(input("Enter the first number: ").strip())
+    num2 = int(input("Enter the second number: ").strip())
+
+    result = num1 * num2
+
+    print(f"{num1} * {num2} = {result}")
+
+    if result > 0:
+        print("This number is positive.")
+    elif result < 0:
+        print("This number is negative.")
+    else:
+        print("This number is both positive and negative.")
+except ValueError:
+    print("Not a number.")
