75f49e141ccdc95ca9eb5214fa8a41f7aa9b8d22
Tuner <127730775+TUNERXD@users.noreply.github.com>
2025-06-24 12:16:39 +0000

    6.3 added greeting_for_all.py

---
+++ cell06/ex03/greetings_for_all.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import sys
+
+
+def main():
+
+    print(greetings('Alexandra'))
+    print(greetings('Wil'))
+    print(greetings())
+    print(greetings(42))
+
+
+def greetings(s = "noble stranger"):
+
+    if not isinstance(s, str):
+        return "Error! It was not a name."
+
+    return f"hello, {s}."
+
+
+if __name__ == "__main__":
+    main()
