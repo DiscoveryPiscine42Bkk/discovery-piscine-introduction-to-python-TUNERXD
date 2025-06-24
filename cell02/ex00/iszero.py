b6137cd3b1e8fa11c7cb94f0bfbf30adfb1422c1
Tuner <127730775+TUNERXD@users.noreply.github.com>
2025-06-24 03:55:44 +0000

    added line #!/usr/bin/env python3

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -1,5 +1,10 @@
-number = int(input(""))
-if number == 0:
-    print("This number is equal to zero.")
-else:
-    print("This number is different from zero.")
+#!/usr/bin/env python3
+
+try:
+    number = int(input(""))
+    if number == 0:
+        print("This number is equal to zero.")
+    else:
+        print("This number is different from zero.")
+except ValueError:
+    print("Not a number")
