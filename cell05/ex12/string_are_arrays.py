cf062b89f043569e220e405ad756e9727adc61db
Tuner <127730775+TUNERXD@users.noreply.github.com>
2025-06-24 09:11:47 +0000

    5.12 added string_are_arrays.py

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+
+if (len(sys.argv) - 1) != 1:
+    print("none")
+    sys.exit(1)
+
+if 'z' not in sys.argv[1]:
+    print("none")
+    sys.exit(1)
+
+for char in sys.argv[1]:
+    if char == 'z':
+        print('z', end = '')
+print()
+
