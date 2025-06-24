b1102a5287ef4c8f0d4312ce309f640ade467ffe
Tuner <127730775+TUNERXD@users.noreply.github.com>
2025-06-24 11:18:12 +0000

    edit the mess

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python3
 import sys
 
-
-def main():
-
-    if len(sys.argv) > 1:
-        print("none")
-        sys.exit(1)
+if len(sys.argv) > 1:
+    print("none")
+    sys.exit(1)
 
 
-    total_table = 10
-    total_num = 10
-    current_table = 0
-    current_num = 0
+total_table = 10
+total_num = 10
+current_table = 0
+current_num = 0
 
-    while current_table <= total_table:
-        print(f"Table de {current_table}: ", end = '')
-        while current_num <= total_num:
-            result = current_table * current_num
-            print(f"{result} ", end = '')
-            current_num += 1
-        current_num = 0 # reset current_num to 0
-        current_table += 1 # increment to next table
-        print()
+while current_table <= total_table:
 
-if __name__ == "__main__":
-    main()
+    print(f"Table de {current_table}: ", end = '')
+
+    while current_num <= total_num:
+
+        print(f"{current_table * current_num} ", end = '')
+        current_num += 1
+
+    current_num = 0 # reset current_num to 0
+    current_table += 1 # increment to next table
+    print()
