7d4102f806527ea920112697ec0cc7b271bfce58
Tuner <127730775+TUNERXD@users.noreply.github.com>
2025-06-24 06:57:47 +0000

    5.1 added play_with_arrays.py

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+ori_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+
+for i in ori_array:
+    new_array.append(i + 2)
+
+print(f"Original array: {ori_array}")
+print(f"New array: {new_array}")
