39394e5f06fb344879c0041d17fb02ab617740a0
Tuner <127730775+TUNERXD@users.noreply.github.com>
2025-06-25 04:37:47 +0000

    Edit into set()

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
 ori_array = [2, 8, 9, 48, 8, 22, -12, 2]
-new_array = []
+new_array = set()
 
 for i in ori_array:
     if i > 5:
-        new_num = i + 2
-        if new_num not in new_array:
-            new_array.append(new_num)
+            new_array.add(i+2)
 
 print(ori_array)
 print(new_array)
